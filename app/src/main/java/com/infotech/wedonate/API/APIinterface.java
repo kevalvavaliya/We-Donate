01b72b1b89c93e3068df40ce35e3a0ccb44c879d
keval vavaliya <kevalvavaliya@gmail.com>
2021-03-09 12:53:11 +0530

    added charity signup

--- app/src/main/java/com/infotech/wedonate/API/APIinterface.java
+++ app/src/main/java/com/infotech/wedonate/API/APIinterface.java
@@ -11,8 +11,11 @@ import retrofit2.http.POST;
 
 public interface APIinterface {
     @POST("/signup_donor")
-    Call<signup_response> signup_donor(@Body signup_data_model user);
+    Call<signup_response> signup_donor(@Body signup_data_model donor);
 
     @POST("/signup_member")
-    Call<signup_response> signup_member(@Body signup_data_model user);
+    Call<signup_response> signup_member(@Body signup_data_model member);
+
+    @POST("/signup_charity")
+    Call<signup_response> signup_charity(@Body signup_data_model charity);
 }
